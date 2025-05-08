8b17b8689dabf3c7de044adf7544807b5abc44f4
Daphne Acher <191528512+DaphneAcher@users.noreply.github.com>
2025-05-08 16:03:29 -0400

    making sure it accepts a list of keywords, implemented regex

--- backend/src/scrapers/greenhouse.py
+++ backend/src/scrapers/greenhouse.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import re
 
 def greenhouse_scraper(company,filters=None):
     if filters is None:
@@ -19,27 +20,43 @@ def greenhouse_scraper(company,filters=None):
         return
     
     for job in data["jobs"]:
+        
         location_match = True
         if "location" in filters:
             location_match = filters["location"].lower() in job["location"]["name"].lower()
 
+        keyword_patterns = []
+
+        if "keywords" in filters:
+            for keyword in filters["keywords"]:
+                keyword = keyword.lower()
+
+                if keyword == "intern":
+                    # Special pattern: match intern, internship, internships, interning
+                    pattern = re.compile(r'\bintern(ship|ships|ing)?\b', re.IGNORECASE)
+                else:
+                    # For other keywords, match the full word
+                    pattern = re.compile(rf'\b{re.escape(keyword)}\b', re.IGNORECASE)
+
+                keyword_patterns.append(pattern)
+
 
         keyword_match = True
-        if "keyword" in filters:
-            keyword_match = filters["keyword"].lower() in job["title"].lower()
+        if keyword_patterns:
+            keyword_match = all(regex_patt.search(job["title"]) for regex_patt in keyword_patterns)
 
-            
-        if location_match and keyword_match:
-            print({company})
-            print(job["title"], "-", job["location"]["name"])
+
+        if keyword_match and location_match:    
+            print(company, job["title"], "-", job["location"]["name"])
 
     
-    
-greenhouse_scraper("spacex", filters={"location":"United States","keyword": "intern"})
-greenhouse_scraper("robinhood", filters={"location":"United States","keyword": "intern"})
-greenhouse_scraper("notion", filters={"location":"United States","keyword": "intern"})
-greenhouse_scraper("stripe", filters={"location":"United States","keyword": "intern"})
-greenhouse_scraper("airbnb", filters={"location":"United States","keyword": "intern"})
-greenhouse_scraper("asana", filters={"location":"United States","keyword": "intern"})
-greenhouse_scraper("cloverhealth", filters={"location":"United States","keyword": "intern"})
-greenhouse_scraper("databricks", filters={"location":"United States", "keyword": "intern"})
+greenhouse_scraper("spacex", filters={"keywords": ["intern","engineering"]})    
+greenhouse_scraper("robinhood", filters={"keywords": ["intern"]})
+greenhouse_scraper("notion", filters={"keywords": ["intern"]})
+greenhouse_scraper("stripe", filters={"keywords": ["intern"]})
+greenhouse_scraper("airbnb", filters={"keywords": ["intern"]})
+greenhouse_scraper("asana", filters={"keywords": ["intern"]})
+greenhouse_scraper("cloverhealth", filters={"keywords": ["intern"]})
+greenhouse_scraper("databricks", filters={"keywords": ["intern"]})
+
+ 
