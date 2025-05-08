18039c99f4b4c1ec7785e0ee32518483e6e2e93b
Daphne Acher <191528512+DaphneAcher@users.noreply.github.com>
2025-05-08 17:11:47 -0400

    add job-type function, make sure we immidately filter out non-intern/new grad postions

--- backend/src/scrapers/greenhouse.py
+++ backend/src/scrapers/greenhouse.py
@@ -2,6 +2,16 @@ import requests
 import json
 import re
 
+def job_type(title):
+    title = title.lower()
+    if re.search(r'\bintern(ship|ships|ing)?\b', title) or re.search(r'\bco[- ]?op\b', title):
+        return "internship"
+    elif re.search(r'\bnew grad(uate)?\b', title) or re.search(r'\bearly career\b', title):
+        return "new grad"
+    else:
+        return "other"
+
+
 def greenhouse_scraper(company,filters=None):
     if filters is None:
         filters = {}
@@ -15,11 +25,16 @@ def greenhouse_scraper(company,filters=None):
 
     data = response.json()
 
+
     if "jobs" not in data:
         print(f"No jobs found at {company}")
         return
     
     for job in data["jobs"]:
+
+        classify = job_type(job("title"))
+        if classify == "other":
+            continue
         
         location_match = True
         if "location" in filters:
@@ -28,17 +43,12 @@ def greenhouse_scraper(company,filters=None):
         keyword_patterns = []
 
         if "keywords" in filters:
-            for keyword in filters["keywords"]:
-                keyword = keyword.lower()
+          for keyword in filters["keywords"]:
+              keyword = keyword.lower()
+              pattern = re.compile(rf'\b{re.escape(keyword)}\b', re.IGNORECASE)
+              keyword_patterns.append(pattern)
 
-                if keyword == "intern":
-                    # Special pattern: match intern, internship, internships, interning
-                    pattern = re.compile(r'\bintern(ship|ships|ing)?\b', re.IGNORECASE)
-                else:
-                    # For other keywords, match the full word
-                    pattern = re.compile(rf'\b{re.escape(keyword)}\b', re.IGNORECASE)
-
-                keyword_patterns.append(pattern)
+              
 
 
         keyword_match = True
@@ -52,7 +62,7 @@ def greenhouse_scraper(company,filters=None):
     
 # greenhouse_scraper("spacex", filters={"keywords": ["intern","engineering"]})    
 # greenhouse_scraper("robinhood", filters={"keywords": ["intern"]})
-greenhouse_scraper("notion", filters={"location":"New York", "keywords": ["intern"]})
+# greenhouse_scraper("notion", filters={"location":"New York", "keywords": ["intern"]})
 # greenhouse_scraper("stripe", filters={"keywords": ["intern"]})
 # greenhouse_scraper("airbnb", filters={"keywords": ["intern"]})
 # greenhouse_scraper("asana", filters={"keywords": ["intern"]})
